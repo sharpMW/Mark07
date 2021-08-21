d6f1e3da50b1695090be2f0cb102d07504dbc866
sharpMW <88919653+sharpMW@users.noreply.github.com>
2021-08-21 23:09:09 +0530

    Updated CSS

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
 var outputDiv = document.querySelector("#output");
-var serverURL = "https://api.funtranslations.com/translate/yoda.json";
 
-function getTranslationURL(input) { 
+var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+
+function getTranslationURL(input) {
     return serverURL + "?" + "text=" + input
 }
 
-
-function errorHandler(error) { 
-    console.log("Error occured", error);
-    alert("Something went wrong")
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
 }
+
 function clickHandler() {
-   var inputText = txtInput.value;
+    var inputText = txtInput.value; 
 
     fetch(getTranslationURL(inputText))
-   .then(response => response.json())
-   .then(json => {
-       var translatedText = json.contents.translated;
-       outputDiv.innertext = translatedText;})
-   .catch(errorHandler)
-   };
-   btnTranslate.addEventListener("click", clickHandler);
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; 
+           })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
